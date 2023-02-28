f6bdb48b9f43fd96086ab7dfbd784d7cc9c0fbbc
emillindblad <emil@lindblad.tech>
2023-02-28 20:42:59 +0100

    started working on db connection

--- flask-project/app.py
+++ flask-project/app.py
@@ -1,6 +1,37 @@
+import os
 from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy.sql import func
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+db = SQLAlchemy()
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, '/db/db.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+
+print(app.config)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50),unique=True, nullable=False)
+    firstname = db.Column(db.String(50), nullable=False)
+    lastname = db.Column(db.String(50), nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True),server_default=func.now())
+    bio = db.Column(db.Text)
+
+    def __repr__(self) -> str:
+        return f'<User {self.firstname}>'
+
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def hello_world():
