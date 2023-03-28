40fabefb7fecb2349c1292c7a503cf54710a5cb1
emillindblad <emil@lindblad.tech>
2023-03-28 11:38:56 +0200

    more work

--- Challenges/command-injection/app.py
+++ Challenges/command-injection/app.py
@@ -9,7 +9,16 @@ def index():
     if request.method == 'POST':
         input = request.form['man']
         command = f"man {input}"
-        output = subprocess.run(command, shell=True,capture_output=True)
-        return render_template('index.html', data=output)
+        result = subprocess.run(command, shell=True,capture_output=True)
+        if result.returncode != 0:
+            error = f'No manual entry for {input}'
+            return render_template('index.html', data=error)
+        output = result.stdout
+
+        # Decode the byte string into a string and replace escape codes with HTML tags
+        parsed = output.decode('utf-8').replace('\n', '<br>').replace('\t', '&nbsp;&nbsp;&nbsp;&nbsp;').replace('\x08', '')
+
+
+        return render_template('index.html', data=parsed)
 
     return render_template('index.html')
