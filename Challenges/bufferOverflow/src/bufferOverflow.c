1d6be6d9a4b36bc2ec66e71be589749f35774f40
Tim Kinderby <tim@kinderby.lamm.com>
2023-04-24 13:55:34 +0200

    Fix password file instead of hardcoded

--- Challenges/bufferOverflow/src/bufferOverflow.c
+++ Challenges/bufferOverflow/src/bufferOverflow.c
@@ -2,18 +2,45 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define PASSWORD "password123"
+#define PASSWORD_FILE "password.txt"
 #define FLAG_FILE "flag.txt"
+
+int read_password_from_file(char *password, const char *filename, size_t buffer_size) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        return 1;
+    }
+
+    fgets(password, buffer_size, file);
+    fclose(file);
+
+    // Remove the newline character if it's present
+    size_t len = strlen(password);
+    if (len > 0 && password[len - 1] == '\n') {
+        password[len - 1] = '\0';
+    }
+
+    return 0;
+}
 
 int main() {
     setbuf(stdout, NULL); // Add this line to disable output buffering
     char input[64];
     char flag[64];
+    char password[64];
     int is_authenticated = 0;
+
+    if (read_password_from_file(password, PASSWORD_FILE, sizeof(password))) {
+        printf("Error: Cannot open password file.\n");
+        return 1;
+    }
 
     printf("Enter password: ");
     gets(input);
 
+    if (strcmp(input, password) == 0) {
+        is_authenticated = 1;
+    }
     if (is_authenticated) {
         FILE *file = fopen(FLAG_FILE, "r");
         if (file == NULL) {
@@ -31,4 +58,3 @@ int main() {
 
     return 0;
 }
-
